b55b82ca2c9ec27e862f9740fecd4257e7ed098e
yuhuajing <18852897525@163.com>
2023-09-21 18:20:37 +0800

    update

--- 10_sign.js
+++ 10_sign.js
@@ -2,7 +2,7 @@ import {ethers} from"ethers";
 
 const main = async () => {
     const provider = new ethers.JsonRpcProvider(`https://cloudflare-eth.com`);
-    const signer = new ethers.Wallet("797391c7bd2e156e52329ceb6471496798e0c125ef35c4c3393329bd2a64f3f5",provider)
+    const signer = new ethers.Wallet("f07a77cb019764a524dce24cb47ac62bb231b4f0d7bab5f864f603f8cb0e344c",provider)
     console.log(`私钥钱包地址:${signer.address}`)
     const message = "Hello, World!";
 
